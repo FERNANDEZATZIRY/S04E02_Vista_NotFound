42aa1bacaa05525a8800492c51b7c28b0bf8dad6
FERNANDEZATZIRY <124528995+FERNANDEZATZIRY@users.noreply.github.com>
2023-05-23 07:58:36 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+// Importando Express
+import express from 'express';
+
+//Importando http-status
+import httpStatus from 'http-status';
+
+// Importando el enrutador
+import adminRouter from './routes/admin.route.js';
+import shopRouter from './routes/shop.route.js';
+
+// Creando la instancia de express
+// que basicamente es un middleware
+const app = express();
+
+// Se registra el middleware del body-parser
+app.use(express.urlencoded({ extended: true }));
+
+// Se agrega ruta de administrador
+app.use(adminRouter);
+// Se agrega ruta shop
+app.use(shopRouter);
+
+
+//Registrando middleware para el error 404 
+app.use((req, res) => {
+    res.sendFile(path.resolve('views', 'error404.html'));
+});
+
+// Definiendo puertos
+const port = 3000;
+const ip = "0.0.0.0"
+
+// Arrancando el servidor
+app.listen(port, ip, () => {
+    console.log(`🤖 Sirviendo en http://localhost:${port}`);
+});
